168f0dee1a6c26bb4fae96ad41ec6ea9e8aa0485
AchrefHamdouni <113561979+AchrefHamdouni@users.noreply.github.com>
2023-05-19 18:32:50 +0100

    update store navbar and footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersComponent } from './components/customers/customers.component';
@@ -22,11 +23,18 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { StoreComponent } from './pages/store/store.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { ManageProductsComponent } from './components/manage-products/manage-products.component';
+import { ManageCategoriesComponent } from './components/manage-categories/manage-categories.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import {CloudinaryModule} from '@cloudinary/ng';
+import { UsersComponent } from './components/users/users.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { CarouselModule } from 'ngx-owl-carousel-o'; // Import OwlCarouselModule
+import { CarouselModule } from 'ngx-owl-carousel-o';
 import { RouterModule } from '@angular/router';
-import { OwlModule } from 'ngx-owl-carousel'; // Add this line
+import { OwlModule } from 'ngx-owl-carousel';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -43,13 +51,19 @@ import { OwlModule } from 'ngx-owl-carousel'; // Add this line
     OrdersComponent,
     OrderItemsComponent,
     ProductsComponent,
+    ProductItemComponent,
     StripeContainerComponent,
     AboutComponent,
     HomeComponent,
     LoginComponent,
     SignupComponent,
     StoreComponent,
-    AddProductComponent
+    AddProductComponent,
+    ManageProductsComponent,
+    ManageCategoriesComponent,
+    AddCategoryComponent,
+    UsersComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
@@ -58,10 +72,11 @@ import { OwlModule } from 'ngx-owl-carousel'; // Add this line
     HttpClientModule,
     RouterModule, // Add RouterModule
     CarouselModule, // Add OwlCarouselModule to the imports array
-    
-
+    ReactiveFormsModule,
+    CloudinaryModule,
+    CommonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
